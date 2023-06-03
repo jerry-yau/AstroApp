096a28d5f37b83a81be63359e0df47924d939d8f
jerry-yau <jerry.yau.2004@gmail.com>
2023-06-03 16:40:31 +0100

    Initial Frame and Contact

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask, render_template
-from datetime import datetime
+import os
+from flask import Flask, render_template, request, redirect
+import datetime
 import re
 
 app = Flask(__name__)
 
-@app.route('/')
+# Make sure API key is set
+if not os.environ.get("NASA_API_KEY"):
+    raise RuntimeError("NASA_API_KEY not set")
+
+@app.route("/")
 def home():
-    return "hello, world."
+    """Homepage of App that contains relevant information"""
+
+    return render_template("index.html")
+
+@app.route("/calculate", methods=["GET", "POST"])
+def calculate():
+    """Takes user info and outputs results on page"""
+
+    return render_template("calculate.html")
+
+@app.route("/bookmark", methods=["GET", "POST"])
+def bookmark():
+    """Bookmark function that saves their input in database and also access their saved information"""
+
+    return render_template("bookmark.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """Contact form to contact the owner of site. Currently saves information in a database."""
+
+    if request.method == "POST":
+        
+
+        return render_template("contact_submitted.html")
+
+    return render_template("contact.html")
